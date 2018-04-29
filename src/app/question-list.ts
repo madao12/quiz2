2c1edab1ba5adecfa64a19f07db915d709c100b8
unknown <lefteris4g@gmail.com>
2018-04-30 02:15:22 +0300

    t2

--- src/app/question-list.ts
+++ src/app/question-list.ts
@@ -3,7 +3,14 @@ import { QuestionClass } from './question';
 export const QUESTIONS: QuestionClass[] = [
     {id:1, name: '2 + 2 is?', ans1: ' 4 ', ans2: '10 ', correctAns: '1'},
     {id:2, name: '4 - 1 is?', ans1: ' 1 ', ans2: '3 ', correctAns: '2'},
-    {id:3, name: 'Do you know the way?', ans1: ' Yes ', ans2: 'No', correctAns: '1'}
+    {id:3, name: 'Do you know the way?', ans1: ' Yes ', ans2: 'No', correctAns: '1'},
+    {id:4, name: '4 + 1 is?', ans1: ' 5 ', ans2: '3 ', correctAns: '1'},
+    {id:5, name: '4 + 11 is?', ans1: ' 15 ', ans2: '3 ', correctAns: '1'},
+    {id:6, name: '5 - 1 is?', ans1: ' 4 ', ans2: '3 ', correctAns: '1'},
+    {id:7, name: '7 - 1 is?', ans1: ' 1 ', ans2: '6 ', correctAns: '2'},
+    {id:8, name: '9 - 1 is?', ans1: ' 8 ', ans2: '3 ', correctAns: '1'},
+    {id:9, name: '8 - 1 is?', ans1: '7 ', ans2: '3 ', correctAns: '1'},
+    {id:10, name: '4 + 6 is?', ans1: ' 7 ', ans2: '10 ', correctAns: '2'}
     
     
     
